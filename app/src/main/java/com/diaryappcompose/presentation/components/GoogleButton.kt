089e2c7670a3e766fb1054360fb2e9af054c7a11
Joshua Jeremia <joshua.jeremia@hotmail.com>
2023-03-29 21:03:47 +0700

    Update dependencies version.

---
+++ app/src/main/java/com/diaryappcompose/presentation/components/GoogleButton.kt
@@ -0,0 +1,27 @@
+package com.diaryappcompose.presentation.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GoogleButton(
+    modifier: Modifier = Modifier,
+    loadingState: Boolean = false,
+    primaryText: String = "Sign in with Google",
+    secondaryText: String = "Please wait...",
+//    icon: Int = R.drawable.icon,
+    shape: Shape = Shapes().extraSmall,
+    borderColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    borderStrokeWidth: Dp = 1.dp,
+    progressIndicatorColor: Color = MaterialTheme.colorScheme.primary,
+    onClick: () -> Unit
+) {
+
+}
